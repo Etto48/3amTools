c53015f2fbba83c8f9a860793571597a8f519a0d
Ettore Ricci <ettore@localhost.localdomain>
2020-04-24 20:43:28 +0200

    Added program to factorize ints

---
+++ Utils/factorize.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import math
+import sys,argparse
+
+parser=argparse.ArgumentParser()
+
+parser.add_argument("--number","-n",required=True,help="Number to factorize",type=int)
+args=parser.parse_args(sys.argv[1:])
+
+n=args.number
+
+pi=[2]
+
+def addPrime(pl):
+    np=pl[len(pl)-1]+1
+    while(1):
+        upper=(math.isqrt(np)+1)
+        fail=False
+        for p in pl:
+            if np%p==0:
+                fail=True
+                break
+            if p>upper:
+                break
+        if fail==False:
+            pl.append(np)
+            return pl
+        np+=1
+    
+def factorize(num,pl):
+    f=pl[len(pl)-1]
+    upper=(math.isqrt(num)+1)
+    while f<upper:
+        print(f"bits: {int(math.log2(f))+1}   f: {f}    {f/upper*100:0.4f}%                  ",end="\r",flush=True)
+        f=pl[len(pl)-1]
+        if num%f==0:
+            print()
+            print(f"found a new factor: {f}")
+            return [f]+factorize(num//f,pl)
+        pl=addPrime(pl)
+    return []
+
+print(factorize(n,pi))
